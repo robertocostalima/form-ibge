ef3995468e3cc09fbdf6335c0529747c4be8dd71
SergioFerreiraFilho <robertogurgel@gmail.com>
2022-09-19 19:51:55 -0300

    Form de buscar cidades do IBGE

---
+++ eventos.js
@@ -0,0 +1,34 @@
+const URL_REGIOES = 'https://servicodados.ibge.gov.br/api/v1/localidades/regioes';
+const URL_ESTADOS = (id) => `https://servicodados.ibge.gov.br/api/v1/localidades/regioes/${id}/estados`;
+const URL_MUNICIPIOS = (id) => `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${id}/municipios`;
+
+fetch(URL_REGIOES)
+    .then(response => response.json())
+    .then(data => {
+        data.map(cadaResult => {
+            regiao.innerHTML += `<option value="${cadaResult.id}">${cadaResult.nome}</option>`;
+        });
+
+    });
+
+regiao.addEventListener('change', () => {
+    estado.innerHTML = '<option>-- Selecione --</option>'
+    fetch(URL_ESTADOS(regiao.value))
+        .then(response => response.json())
+        .then(dados => {
+            dados.map(est => {
+                estado.innerHTML += `<option value="${est.id}">${est.nome}</option>`;
+            })
+        });
+});
+
+estado.addEventListener('change', () => {
+    cidade.innerHTML = '<option>-- Selecione --</option>'
+    fetch(URL_MUNICIPIOS(estado.value))
+        .then(response => response.json())
+        .then(dados => {
+            dados.map(cid => {
+                cidade.innerHTML += `<option value="${cid.id}">${cid.nome}</option>`;
+            })
+        });
+}); //select2 --> https://select2.org/
